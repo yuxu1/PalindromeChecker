1c21ece40c1671c3db85c26908ccbb3ff9edda45
yuxu1 <yufanx8@gmail.com>
2015-01-12 12:18:54 -0800

    Added code to PalindromeChecker.pde

--- build-tmp/source/PalindromeChecker.java
+++ build-tmp/source/PalindromeChecker.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class PalindromeChecker extends PApplet {
+
 public void setup()
 {
   String lines[] = loadStrings("palindromes.txt");
@@ -39,3 +55,12 @@ public boolean palindrome(String word)
   return true;
 }
   
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "PalindromeChecker" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
